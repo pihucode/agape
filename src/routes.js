983f2a0bf96356d9028cd875ae7bac74cb954199
Pinxuan Huang <pinxhuang@gmail.com>
2019-09-21 09:17:33 -0400

    fix merge issues routes

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,11 @@
 import Welcome from './components/Welcome'
 import Featured from './components/Featured'
-<<<<<<< HEAD
 import Signin from './components/Signin'
-=======
 import MyPets from './components/MyPets'
->>>>>>> b189f50d2d58a4a6a8d8a46d45f4a4ac6c6f483f
 
 export default [
   { path: '/', component: Welcome },
   { path: '/featured', component: Featured },
-<<<<<<< HEAD
   { path: '/signin', component: Signin },
-=======
   { path: '/mypets', component: MyPets },
->>>>>>> b189f50d2d58a4a6a8d8a46d45f4a4ac6c6f483f
 ]
