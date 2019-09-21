ef7e9d71e1186b74e77345671bab04576f229ca9
Pinxuan Huang <pinxhuang@gmail.com>
2019-09-21 15:29:23 -0400

    cors

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -17,15 +17,64 @@
 
 <script>
 import AdoptModule from "./AdoptModule";
+// import * as auth2 from "https://apis.google.com/js/platform.js";
 
 export default {
   components: {
     "adopt-module": AdoptModule
   },
+  created() {
+    console.log("created() function called");
+    // get current logged in user's email and set as currentUsername
+    this.currentUsername = localStorage.getItem("username");
+    console.log("username: " + this.currentUsername);
+
+    this.checkUser();
+  },
   data() {
     return {
-      // title: "Sign "
+      currentUsername: ""
     };
+  },
+  methods: {
+    // addUser() {
+    //   this.$http
+    //     .request({
+    //       url:
+    //         "https://agape-api.herokuapp.com/new-user/" + this.currentUsername,
+    //       method: "POST"
+    //       // headers: { "Content-Type": "application/json" },
+    //       // content: JSON.stringify({
+    //       //   username: "username",
+    //       //   password: "password"
+    //       // })
+    //     })
+    //     .then(
+    //       response => {
+    //         var result = response.content.toJSON();
+    //         console.log("POSTED SUCCESSFULLY");
+    //         console.log(result);
+    //       },
+    //       error => {
+    //         console.error(error);
+    //       }
+    //     );
+    // },
+    checkUser() {
+      this.$http
+        .get("https://agape-api.herokuapp.com/user/" + this.currentUsername)
+        .then(function(data) {
+          // add user if they are not in DB
+          // if (!Object.keys(response.data).length) {
+          console.log("get response succeeded");
+          console.log(data);
+          if (data.username == "not found") {
+            console.log("add user");
+          } else {
+            console.log("else boop");
+          }
+        });
+    }
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueResourece from 'vue-resource'
+import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
 import Routes from './routes'
 import BootstrapVue from 'bootstrap-vue'
+import GoogleAuth from 'vue-google-auth'
 
-Vue.use(VueResourece)
+Vue.use(VueResource)
 Vue.use(VueRouter)
 Vue.use(BootstrapVue)
+Vue.use(GoogleAuth, { clientID: '678587520258-a2ocgf9mblrtv6offpbotmcip6bf6v7f.apps.googleusercontent.com' })
+Vue.googleAuth().load()
 
 const router = new VueRouter({
   routes: Routes,
