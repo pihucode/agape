826db9782cf684ed0282560678dbe31db1bcdaaa
Amy Wang <horselurrver@gmail.com>
2019-09-21 07:44:28 -0400

    Add route for mypets.vue

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import Home from './components/Home'
 import Featured from './components/Featured'
+import MyPets from './components/MyPets'
 
 export default [
   { path: '/', component: Home },
   { path: '/featured', component: Featured },
+  { path: '/mypets', component: MyPets },
 ]
