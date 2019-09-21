93bf4c9b15811f96ef8ed1ccd6d4c880ec33a77d
Pinxuan Huang <pinxhuang@gmail.com>
2019-09-21 16:14:35 -0400

    module api

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -37,41 +37,66 @@ export default {
     };
   },
   methods: {
-    // addUser() {
-    //   this.$http
-    //     .request({
-    //       url:
-    //         "https://agape-api.herokuapp.com/new-user/" + this.currentUsername,
-    //       method: "POST"
-    //       // headers: { "Content-Type": "application/json" },
-    //       // content: JSON.stringify({
-    //       //   username: "username",
-    //       //   password: "password"
-    //       // })
-    //     })
-    //     .then(
-    //       response => {
-    //         var result = response.content.toJSON();
-    //         console.log("POSTED SUCCESSFULLY");
-    //         console.log(result);
-    //       },
-    //       error => {
-    //         console.error(error);
-    //       }
-    //     );
-    // },
-    checkUser() {
+    addUser() {
       this.$http
-        .get("https://agape-api.herokuapp.com/user/" + this.currentUsername)
+        .request({
+          url:
+            "https://agape-api.herokuapp.com/new-user/" + this.currentUsername,
+          method: "POST"
+          // headers: { "Content-Type": "application/json" },
+          // content: JSON.stringify({
+          //   username: "username",
+          //   password: "password"
+          // })
+        })
+        .then(
+          response => {
+            var result = response.content.toJSON();
+            console.log("POSTED SUCCESSFULLY");
+            console.log(result);
+          },
+          error => {
+            console.error(error);
+          }
+        );
+    },
+    checkUser() {
+      // this.$http
+      //   .get("https://agape-api.herokuapp.com/user/" + this.currentUsername, {
+      //     headers: {
+      //       // "Access-Control-Allow-Origin": "*",
+      //       "Access-Control-Allow-Methods": "POST, GET, PUT, OPTIONS, DELETE",
+      //       "Access-Control-Allow-Headers":
+      //         "Access-Control-Allow-Methods, Access-Control-Allow-Origin, Origin, Accept, Content-Type",
+      //       "Content-Type": "application/json",
+      //       Accept: "application/json"
+      //     }
+      //   })
+      //   .then(
+      //     function(response) {
+      //       console.log("success");
+      //     },
+      //     function(response) {
+      //       // Error
+      //       console.log("error");
+      //     }
+      //   );
+      this.$http
+        .get(
+          "https://cors-anywhere.herokuapp.com/https://agape-api.herokuapp.com/user/" +
+            this.currentUsername
+        )
         .then(function(data) {
           // add user if they are not in DB
           // if (!Object.keys(response.data).length) {
           console.log("get response succeeded");
           console.log(data);
-          if (data.username == "not found") {
+          if (data.body.username == "not found") {
+            this.addUser();
             console.log("add user");
           } else {
             console.log("else boop");
+            console.log(data.body.username);
           }
         });
     }
