b36db3b5d31d9fa95c7466903fe4f7cc7fa0a5d3
Amy Wang <horselurrver@gmail.com>
2019-09-21 19:10:22 -0400

    Finish bulletin

--- src/components/Featured.vue
+++ src/components/Featured.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <h2>Featured</h2>
+    <img id="bulletin" src="../assets/bulletin.png" />
+    <div class="column">
+      <a href="https://www.salvationarmyusa.org/usn/">
+        <img id="salvation" class="postit" src="../assets/salvation.png" />
+      </a>
+      <a href="https://www.stjude.org/">
+        <img class="postit" src="../assets/stjudes.png" />
+      </a>
+      <a href="https://www.habitat.org/">
+        <img class="postit" src="../assets/habitat.png" />
+      </a>
+    </div>
   </div>
 </template>
 
@@ -16,6 +27,34 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-div {
+.column {
+  margin-top: 15%;
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  margin-left: auto;
+  margin-right: auto;
+  left: 0;
+  right: 0;
+  align-items: center;
+}
+
+#bulletin {
+  width: 50%;
+  position: absolute;
+  margin-left: auto;
+  margin-right: auto;
+  left: 0;
+  right: 0;
+  margin-bottom: 10em;
+}
+
+.postit {
+  transition: 0.3s ease;
+  width: 40%;
+}
+
+.postit:hover {
+  transform: scale(1.2);
 }
 </style>
